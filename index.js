f22f4e73813d1181d8a6d91393d2f40511e37b8e
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-02-27 15:59:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function subtract(a, b){
     return (n-1)}
  function makeInt(n){
     return (parseInt(n,10))}
-function preserveDecimal (n)
- return 
+function preserveDecimal (n){
+ return (parseFloat(n))}
+ 
     
